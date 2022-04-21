23db874a93b3c30485c4b4ba1c2b235caa5231ef
Gelnior <gelnior@free.fr>
2022-04-22 00:00:55 +0200

    Add Lore to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { HomeView } from "../views";
+import Image from "next/image"
 
 const Home: NextPage = (props) => {
   return (
@@ -12,10 +12,34 @@ const Home: NextPage = (props) => {
           content="The home of the Happy Hippos community"
         />
       </Head>
-      <h1 className="max-w-screen-lg m-auto text-center funny text-6xl mt-8">
-        Welcome to the Happy Hippos!
-      </h1>
-    </div>
+      <div className="max-w-screen-lg m-auto text-center funny">
+        <h1 className="text-6xl mt-24">
+          Welcome to the Happy Hippos!
+        </h1>
+        <h2 className="text-5xl mt-16">Discover our Lore</h2>
+        <p className="mt-8">
+          <Image
+            className="rounded-lg"
+            src="/lore/lore_01.png"
+            alt="Hippo #{hippoid} picture"
+            height="400"
+            width="600"
+          />
+        </p>
+        <p className="mt-8">
+          <Image
+            className="rounded-lg"
+            src="/lore/lore_02.png"
+            alt="Hippo #{hippoid} picture"
+            height="400"
+            width="600"
+          />
+        </p>
+        <p className="mt-8 mb-8">
+          More coming soon...
+        </p>
+      </div>
+    </div >
   );
 };
 
